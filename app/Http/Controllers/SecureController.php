b06802683a7b3bd432d0621bc36e2ce0a2709d32
goopitt <goopitt@gmail.com>
2020-10-29 17:49:06 +0700

    Add files via upload

--- app/Http/Controllers/SecureController.php
+++ app/Http/Controllers/SecureController.php
@@ -1,12 +1,28 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class SecureController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
 	public function profile(Request $request)
 	{
-		
+        $user = User::where('token', $request)->get();
+        if ($user) {
+              $res['success'] = true;
+              $res['message'] = $user;
+        
+              return response($res);
+        }else{
+          $res['success'] = false;
+          $res['message'] = 'Cannot find user!';
+        
+          return response($res);
+        }
 	}
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,17 +1,30 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Phone;
+use App\Address;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
-    public function index(Request $request)
+    public function showAllUsers()
     {
-        
+        // dd("test");
+        return response()->json(User::all());
     }
 
-    public function view(Request $request, string $user)
+    public function showOneUser($id)
     {
-
+        //dd($id);
+        return response()->json(User::find($id));
     }
+
+    public function showRelationUser($phone)
+    {
+        //dd($id);
+        return response()->json(User::find($id));
+    }
+
 }
